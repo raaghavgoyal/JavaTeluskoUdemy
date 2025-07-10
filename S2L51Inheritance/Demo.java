eba650df0014ac0cd8ab7128134e0fb86f310fed
Raghav <raaghavgoyal@gmail.com>
2025-07-10 08:51:03 +0530

    Multilevel Inheritance

--- S2L51Inheritance/Demo.java
+++ S2L51Inheritance/Demo.java
@@ -2,11 +2,14 @@ package S2L51Inheritance;
 
 public class Demo {
     public static void main(String[] args) {
-        AdvCalc c = new AdvCalc();
+        VeryAdvCalc c = new VeryAdvCalc();
         int r1 = c.add(1, 5);
         int r2 = c.sub(7, 3);
         int r3 = c.multi(2, 33);
         int r4 = c.div(15, 4);
-        System.out.println(r1+" "+r2+" "+r3+" "+r4);
+        double r5 = c.power(4, 2);
+
+        System.out.println(r1+" "+r2+" "+r3+" "+r4+" "+r5);
+
     }
 }
