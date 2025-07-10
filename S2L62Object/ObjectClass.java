2a8aa17336602c79b8c8d1d6170d13ccb93dd609
Raghav <raaghavgoyal@gmail.com>
2025-07-10 18:04:24 +0530

    Object Class

---
+++ S2L62Object/ObjectClass.java
@@ -0,0 +1,56 @@
+package S2L62Object;
+
+class Laptop{
+    String model;
+    int price;
+    
+    @Override
+    public String toString() {
+        return "Laptop [model=" + model + ", price=" + price + "]";
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + price;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    }
+    
+}
+public class ObjectClass {
+    public static void main(String[] args) {
+        Laptop obj1 = new Laptop();
+        obj1.model = "Lenovo Yoga";
+        obj1.price = 1000;
+
+        Laptop obj2 = new Laptop();
+        obj2.model = "Lenovo Yoga";
+        obj2.price = 1000;
+
+        boolean result = obj1.equals(obj2);
+
+        System.out.println(result);
+        System.out.println(obj1);
+        System.out.println(obj2);;
+    }
+    
+}
