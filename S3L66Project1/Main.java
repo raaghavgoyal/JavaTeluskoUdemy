81154e983f4a69f73cea4b8be9fa419b1b06e302
Raghav <raaghavgoyal@gmail.com>
2025-07-11 15:15:37 +0530

    Project 1 Quiz

---
+++ S3L66Project1/Main.java
@@ -0,0 +1,10 @@
+package S3L66Project1;
+
+public class Main {
+    public static void main(String[] args) {
+        QuestionService service = new QuestionService();
+        service.playQuiz();
+        service.printScore();
+    }
+    
+}
