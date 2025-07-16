0f327170b7dbda6016ecf3c98398c60f04f4d266
Raghav <raaghavgoyal@gmail.com>
2025-07-16 08:00:30 +0530

    Minor changes to resolve errors

--- S3L94Threads/Demo.java
+++ S3L94Threads/Demo.java
@@ -1,13 +1,13 @@
 package S3L94Threads;
 
-class A extends ThreadAndRunnable{
+class A extends Thread{
     //we must have a run method in a class which extends Tread class, 
     //so that it can execute when we say obj.start();
     public void run(){
         for(int i = 1; i<=100; i++){
             System.out.println("hi");
             try {
-                ThreadAndRunnable.sleep(10);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -15,12 +15,12 @@ class A extends ThreadAndRunnable{
     }
 }
 
-class B extends ThreadAndRunnable{
+class B extends Thread{
     public void run(){
         for(int i = 1; i<=100; i++){
             System.out.println("hello");
             try {
-                ThreadAndRunnable.sleep(10);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -34,10 +34,10 @@ public class Demo {
         B obj2 = new B();
 
         System.out.println(obj1.getPriority());
-        obj2.setPriority(ThreadAndRunnable.MAX_PRIORITY);
+        obj2.setPriority(Thread.MAX_PRIORITY);
 
         obj1.start();
-        ThreadAndRunnable.sleep(2);
+        Thread.sleep(2);
         obj2.start();
     }
 }
