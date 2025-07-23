8feab55b5922c5ddc5043b17e34ba9ae41da50fc
Raghav <raaghavgoyal@gmail.com>
2025-07-23 13:49:13 +0530

    HQL- fetch data using hibernate query language

--- HibernateProject/src/main/java/org/example/Main.java
+++ HibernateProject/src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -72,41 +74,41 @@ public class Main {
 //        transaction.commit();
 //        session.close();
 //        sf.close();
-
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Asus");
-        l1.setModel("Rog");
-        l1.setRam(16);
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("Dell");
-        l2.setModel("XPS");
-        l2.setRam(16);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Apple");
-        l3.setModel("Macbook air");
-        l3.setRam(8);
-
-        Aliens a1 = new Aliens();
-        a1.setAid(101);
-        a1.setAname("Navin");
-        a1.setTech("Java");
-
-        Aliens a2 = new Aliens();
-        a2.setAid(102);
-        a2.setAname("Harsh");
-        a2.setTech("Python");
+//
+//        Laptop l1 = new Laptop();
+//        l1.setLid(4);
+//        l1.setBrand("Asus");
+//        l1.setModel("Strix");
+//        l1.setRam(32);
+//        Laptop l2 = new Laptop();
+//        l2.setLid(2);
+//        l2.setBrand("Dell");
+//        l2.setModel("XPS");
+//        l2.setRam(16);
+//
+//        Laptop l3 = new Laptop();
+//        l3.setLid(3);
+//        l3.setBrand("Apple");
+//        l3.setModel("Macbook air");
+//        l3.setRam(8);
+//
+//        Aliens a1 = new Aliens();
+//        a1.setAid(101);
+//        a1.setAname("Navin");
+//        a1.setTech("Java");
+//
+//        Aliens a2 = new Aliens();
+//        a2.setAid(102);
+//        a2.setAname("Harsh");
+//        a2.setTech("Python");
 
 //        Aliens a3 = new Aliens();
 //        a3.setAid(103);
 //        a3.setAname("Kiran");
 //        a3.setTech("AI");
 
-        a1.setLaptops(Arrays.asList(l1,l2));
-        a2.setLaptops(Arrays.asList(l3));
+//        a1.setLaptops(Arrays.asList(l1,l2));
+//        a2.setLaptops(Arrays.asList(l3));
         //a3.setLaptops(Arrays.asList(l1));
 
 //        l1.setAliens(Arrays.asList(a1,a3));
@@ -114,46 +116,54 @@ public class Main {
 //        l3.setAliens(Arrays.asList(a2));
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(org.example.Aliens.class)
+                //.addAnnotatedClass(org.example.Aliens.class)
                 .addAnnotatedClass(org.example.Laptop.class)
                 .configure()
                 .buildSessionFactory();
         Session session = sf.openSession();
 
-        Transaction transaction = session.beginTransaction();
+
+        Query query = session.createQuery("from Laptop where ram = 16");
+        List<Laptop> laptops = query.getResultList();
+        //sql: select * from laptop where ram = 32
+        // hql: from Laptop where ram=32
+        //Laptop l1 = session.get(Laptop.class, 3);
+        System.out.println(laptops);
+
+//        Transaction transaction = session.beginTransaction();
 
         //sequence... because laptop is parent table and alien is child table
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
-
-        session.persist(a1);
-        session.persist(a2);
+//        session.persist(l1);
+//        session.persist(l2);
+//        session.persist(l3);
+//
+//        session.persist(a1);
+//        session.persist(a2);
 //        session.persist(a3);
 
-        transaction.commit();
+//        transaction.commit();
 
 
         //System.out.println(a5);
 
         session.close();
-        Session session1 = sf.openSession();
-
-        Aliens a5 = session1.get(Aliens.class, 101);
-        //the hibernate does not fire a new select query when we write get in the same
-        //session because, hibernate uses CACHE (2 levels l1,l2)
-        //by default hibernate has l1 cache
-        //but it does fire select query in new session
-        //we can also share data in different sessions using l2 cache and libraries
-        //like EH cache and caffeine
-        //lazy fetch(by default)- does not give collections present in the entity
-        //but when we print, we explicitly want complete data
-        // so it executes the complete query
-        //because there may be huge amount of data which may be a expensive task
-        //but we can change this lazy behaviour by specifying fetch type in
-        //one to many mapping
-        //System.out.println(a5);
-        session1.close();
+//        Session session1 = sf.openSession();
+//
+//        Aliens a5 = session1.get(Aliens.class, 101);
+//        //the hibernate does not fire a new select query when we write get in the same
+//        //session because, hibernate uses CACHE (2 levels l1,l2)
+//        //by default hibernate has l1 cache
+//        //but it does fire select query in new session
+//        //we can also share data in different sessions using Jcache(l2 cache) and libraries
+//        //like EH cache and caffeine
+//        //lazy fetch(by default)- does not give collections present in the entity
+//        //but when we print, we explicitly want complete data
+//        // so it executes the complete query
+//        //because there may be huge amount of data which may be a expensive task
+//        //but we can change this lazy behaviour by specifying fetch type in
+//        //one to many mapping
+//        //System.out.println(a5);
+//        session1.close();
 
         sf.close();
 
