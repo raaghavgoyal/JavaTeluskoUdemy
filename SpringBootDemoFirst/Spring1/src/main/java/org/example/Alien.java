31ba4fdcbc716b1818a2074d892160bcc68055c1
Raghav <raaghavgoyal@gmail.com>
2025-07-24 16:25:51 +0530

    constructor injections using index, name and type

--- SpringBootDemoFirst/Spring1/src/main/java/org/example/Alien.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/Alien.java
@@ -1,30 +1,37 @@
 package org.example;
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
 
     private int age;
+    private int salary;
     private Laptop lap;
+
+
     public Alien(){
         System.out.println("Object Created");
+    }
+
+    @ConstructorProperties({"age","lap"})
+    public Alien(int age, Laptop lap) {
+        this.age = age;
+        this.lap = lap;
     }
 
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         System.out.println("Setter called");
         this.age = age;
     }
-
     public Laptop getLap() {
         return lap;
     }
-
     public void setLap(Laptop lap) {
         this.lap = lap;
     }
-
     public void code(){
         System.out.println("Coding");
         lap.compile();
