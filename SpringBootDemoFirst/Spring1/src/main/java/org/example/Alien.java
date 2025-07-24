6e281b836da1ef6d8b9d330267e1c54c89c28531
Raghav <raaghavgoyal@gmail.com>
2025-07-24 15:51:45 +0530

    setter injection in spring

--- SpringBootDemoFirst/Spring1/src/main/java/org/example/Alien.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/Alien.java
@@ -2,11 +2,20 @@ package org.example;
 
 public class Alien {
 
-    int age;
-
+    private int age;
     public Alien(){
         System.out.println("Object Created");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter called");
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("Coding");
     }
--- SpringBootDemoFirst/Spring1/src/main/java/org/example/App.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/App.java
@@ -10,10 +10,10 @@ public class App
         //BeanFactory---- old, ApplicationContext---- new
         //this line creates a container for us
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Alien obj1 = (Alien) context.getBean("alien1");
-//        obj1.age = 21;
-//        System.out.println(obj1.age);
-//        //obj1.code();
+        Alien obj1 = (Alien) context.getBean("alien1");
+        //obj1.setAge(21);
+        System.out.println(obj1.getAge());
+        obj1.code();
 //
 //        Alien obj2 = (Alien) context.getBean("alien1");
 //        //same value, because of singleton scope in spring (by default) (in xml file)
