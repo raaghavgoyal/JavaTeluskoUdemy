edbfb5a4697e06452b86a2990970543f4e35523c
Raghav <raaghavgoyal@gmail.com>
2025-07-25 16:51:48 +0530

    Qualifier and Primary using java annotation

--- SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
@@ -3,13 +3,25 @@ package org.example.config;
 import org.example.Alien;
 import org.example.Computer;
 import org.example.Desktop;
+import org.example.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public Alien alien(Computer com){   //@Qualifier("desktop")
+                                        //@Autowired in not required in new versions
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setCom(com);
+        return obj;
+    }
 
     //to give multiple names to the bean
     //@Bean(name = {"com2","desktop1","Beast"})
@@ -20,10 +32,10 @@ public class AppConfig {
     }
 
     @Bean
-    public Alien alien(@Autowired Computer com){//@Autowired in not required in new versions
-        Alien obj = new Alien();
-        obj.setAge(25);
-        obj.setCom(com);
-        return obj;
+    @Primary
+    public Laptop laptop(){
+        return new Laptop();
     }
+
+
 }
