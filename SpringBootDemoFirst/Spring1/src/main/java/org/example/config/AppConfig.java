715a801f9ef6821ed88a1499ce893ae1ea4ff4fc
Raghav <raaghavgoyal@gmail.com>
2025-07-25 16:20:19 +0530

    Autowire

--- SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
@@ -1,6 +1,9 @@
 package org.example.config;
 
+import org.example.Alien;
+import org.example.Computer;
 import org.example.Desktop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,8 +14,16 @@ public class AppConfig {
     //to give multiple names to the bean
     //@Bean(name = {"com2","desktop1","Beast"})
     @Bean
-    @Scope("prototype")
+    //Scope("prototype")
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    public Alien alien(@Autowired Computer com){//@Autowired in not required in new versions
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setCom(com);
+        return obj;
     }
 }
