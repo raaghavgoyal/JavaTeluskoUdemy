4c4d2013a4c75195d6045674561d2cb273ee4199
Raghav <raaghavgoyal@gmail.com>
2025-07-25 16:01:19 +0530

    Bean Name, default name

--- SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
+++ SpringBootDemoFirst/Spring1/src/main/java/org/example/config/AppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //to give multiple names to the bean
+    //@Bean(name = {"com2","desktop1","Beast"})
     @Bean
     public Desktop desktop(){
         return new Desktop();
