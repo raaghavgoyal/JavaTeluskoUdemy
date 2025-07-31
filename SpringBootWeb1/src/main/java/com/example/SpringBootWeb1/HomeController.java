3839f682a60e147c1cf87469362099ad9f3f6896
Raghav <raaghavgoyal@gmail.com>
2025-07-31 16:29:30 +0530

    display data on result page using session and jstl

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.SpringBootWeb1;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,15 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req){
+    public String add(HttpServletRequest req, HttpSession session){
         //System.out.println("in add");
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1+num2;
-        System.out.println(result);
+        //System.out.println(result);
+
+        session.setAttribute("result",result);
+
         return "result.jsp";
     }
 
