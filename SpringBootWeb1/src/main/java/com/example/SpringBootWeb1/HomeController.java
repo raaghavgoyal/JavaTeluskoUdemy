7ea1b4dba2f1cb932c3d7efa44a98ac2f46a333e
Raghav <raaghavgoyal@gmail.com>
2025-07-31 17:33:14 +0530

    model instead of session object

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -3,6 +3,7 @@ package com.example.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +18,10 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
         int result = num1+num2;
 
-        session.setAttribute("result",result);
+        model.addAttribute("result",result);
 
         return "result.jsp";
     }
