9093f8c8178baba761fccab0d764ce665b886835
Raghav <raaghavgoyal@gmail.com>
2025-07-31 17:46:36 +0530

    ModelAndView instead of separately

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -18,12 +19,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
         int result = num1+num2;
 
-        model.addAttribute("result",result);
+        mv.addObject("result",result);
+        mv.setViewName("result");
 
-        return "result";
+        return mv;
     }
 
 }
