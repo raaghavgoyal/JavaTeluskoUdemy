e875ee341f4c0fbc18609f1a0a727b254c5d9cb6
Raghav <raaghavgoyal@gmail.com>
2025-08-04 13:45:06 +0530

    spring data jpa - creating table and inserting data in it

---
+++ spring-data-jpa-ex/src/main/java/com/example/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -0,0 +1,35 @@
+package com.example.spring_data_jpa_ex;
+
+import com.example.spring_data_jpa_ex.model.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDataJpaExApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringDataJpaExApplication.class, args);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+		Student s1 = context.getBean(Student.class);
+		s1.setRollNo(101);
+		s1.setName("Navin");
+		s1.setMarks(75);
+
+		Student s2 = context.getBean(Student.class);
+		s2.setRollNo(102);
+		s2.setName("Kiran");
+		s2.setMarks(80);
+
+		Student s3 = context.getBean(Student.class);
+		s3.setRollNo(103);
+		s3.setName("Harsh");
+		s3.setMarks(70);
+
+		repo.save(s1);
+
+	}
+
+}
