3983f193b80c55b01b0569f24dbbfb4a73e43b9b
Raghav <raaghavgoyal@gmail.com>
2025-08-05 19:12:32 +0530

    add product with image

--- SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
+++ SpringEcom/src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -30,6 +32,20 @@ public class ProductController   {
             return new ResponseEntity<>(product, HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    //wildcard because response can be eroor message or product
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 }
--- SpringEcom/src/main/java/com/example/SpringEcom/model/Product.java
+++ SpringEcom/src/main/java/com/example/SpringEcom/model/Product.java
@@ -1,10 +1,7 @@
 package com.example.SpringEcom.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,4 +26,13 @@ public class Product {
     private Date releaseDate;
     private boolean product_available;
     private int stockQuantity;
+    //for image part
+    private String imageName;
+    private String imageType;
+    @Lob    //Large Object
+    private byte[] imageData;
+
+    public Product(int id){
+        this.id = id;
+    }
 }
