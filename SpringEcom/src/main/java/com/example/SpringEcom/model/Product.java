e06d3922db7ca42be1781b72547945a256faa501
Raghav <raaghavgoyal@gmail.com>
2025-08-05 17:13:22 +0530

    project setup springboot MVC

---
+++ SpringEcom/src/main/java/com/example/SpringEcom/model/Product.java
@@ -0,0 +1,30 @@
+package com.example.SpringEcom.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//to automatically create the id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean isAvailable;
+    private int stockQuantity;
+}
