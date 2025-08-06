413c1e9575d6dd83575c3f93d47ca8b5529a9838
Raghav <raaghavgoyal@gmail.com>
2025-08-06 22:36:17 +0530

    Spring data rest- get update delete without the controller

---
+++ spring-data-rest-demo/src/main/java/com/example/spring_data_rest_demo/model/JobPost.java
@@ -0,0 +1,26 @@
+package com.example.spring_data_rest_demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+//no need to create getters setters constructors toString or hashCode method because of
+//lombok dependency and these annotations
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+public class JobPost {
+    @Id
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
+}
