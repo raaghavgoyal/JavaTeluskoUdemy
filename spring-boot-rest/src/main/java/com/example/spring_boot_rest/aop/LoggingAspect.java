f3a100b860ed6e7abfe188c6e912b1ae60a21586
Raghav <raaghavgoyal@gmail.com>
2025-08-07 14:15:02 +0530

    using AOP to call the log before executing the methods

--- spring-boot-rest/src/main/java/com/example/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/example/spring_boot_rest/aop/LoggingAspect.java
@@ -1,11 +1,21 @@
 package com.example.spring_boot_rest.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    //return type, class-name.method-name(args)
+    //* -> all the methods
+    //.. -> all the arguments
+    @Before("execution(* com.example.spring_boot_rest.service.JobService.*(..))")
     public void logMethodCall(){
 
         LOGGER.info("Method called");
