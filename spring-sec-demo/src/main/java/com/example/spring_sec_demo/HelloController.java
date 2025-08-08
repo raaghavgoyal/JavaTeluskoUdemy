0b9bb293a741998d164f425a202855d1a433ece3
Raghav <raaghavgoyal@gmail.com>
2025-08-08 18:27:48 +0530

    session id

--- spring-sec-demo/src/main/java/com/example/spring_sec_demo/HelloController.java
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.spring_sec_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String greet(){
-        return "Hello World!";
+    @GetMapping("hello")
+    public String greet(HttpServletRequest request){
+        return "Hello World, " + request.getSession().getId();
     }
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Telusko, " + request.getSession().getId();
+    }
+
 }
--- spring-sec-demo/src/main/java/com/example/spring_sec_demo/SpringSecDemoApplication.java
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/SpringSecDemoApplication.java
@@ -2,6 +2,7 @@ package com.example.spring_sec_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringSecDemoApplication {
