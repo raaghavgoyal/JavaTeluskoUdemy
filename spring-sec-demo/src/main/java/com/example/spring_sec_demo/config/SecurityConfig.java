f1adc5a2f7fa9f23e67c42db556f4ea21439ccd0
Raghav <raaghavgoyal@gmail.com>
2025-08-13 17:50:05 +0530

    without lambda and with lambda,builder pattern

--- spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,12 +16,39 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        without lambda
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//                configurer.disable();
+//            }
+//        };
+//        http.csrf(custCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> custHttp = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//            @Override
+//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+//                registry.anyRequest().authenticated();
+//            }
+//        };
+//        http.authorizeHttpRequests(custHttp);
 
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
-        //http.formLogin(Customizer.withDefaults());    //not required for stateless
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //with lambda
+//        http.csrf(customizer -> customizer.disable());
+//        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+//        //http.formLogin(Customizer.withDefaults());    //not required for stateless
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //with lambda and builder pattern
+        http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request->request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
 
         return http.build();
 
