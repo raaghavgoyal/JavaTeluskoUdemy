095005dda361547cf9624a8b583737abfc864af3
Raghav <raaghavgoyal@gmail.com>
2025-08-13 17:33:37 +0530

    disabling csrf token and making it stateless

--- spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.spring_sec_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+        //http.formLogin(Customizer.withDefaults());    //not required for stateless
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
 
