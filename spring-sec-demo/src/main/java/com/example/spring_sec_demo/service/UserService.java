4500b36c91ed6c65e2d79c76c21c6e77c0dae778
Raghav <raaghavgoyal@gmail.com>
2025-08-14 14:14:58 +0530

    User Registration

---
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.spring_sec_demo.service;
+
+import com.example.spring_sec_demo.dao.UserRepo;
+import com.example.spring_sec_demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    public User saveUser(User user){
+        return repo.save(user);
+    }
+
+}
